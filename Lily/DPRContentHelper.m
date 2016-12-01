9ecbce05a87b9b0b4279efef30483b3ae5783607
shavix <davidpr1994@gmail.com>
2016-11-30 22:14:08 -0800

    cleaned

--- Lily/DPRContentHelper.m
+++ Lily/DPRContentHelper.m
@@ -69,37 +69,37 @@
     NSString *apacheLicense = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
     
     NSMutableString *content = [[NSMutableString alloc] init];
-    [content appendString:@"\nImages\n\n"];
-    [content appendString:@"Madebyoliver: Presentation\n"];
-    [content appendString:@"Madebyoliver: Graph\n"];
-    [content appendString:@"Madebyoliver: Info\n"];
-	[content appendString:@"Madebyoliver: Financials\n"];
-	[content appendString:@"Madebyoliver: User\n"];
-    [content appendString:@"Freepik: Athletics\n"];
-    [content appendString:@"Freepik: Agreement\n"];
-    [content appendString:@"Freepik: Businessman\n"];
-    [content appendString:@"Freepik: Lily\n"];
-	[content appendString:@"Freepik: Suitcase-with-money"];
-    [content appendString:@"Freepik: Bonusprofit\n"];
-    [content appendString:@"Freepik: Cashpayment\n"];
-    [content appendString:@"Freepik: Fiscalyear\n"];
-    [content appendString:@"Freepik: Taxform\n"];
-    [content appendString:@"Roundicons: Free\n"];
-    [content appendString:@"Roundicons: Info\n"];
-    [content appendString:@"\n----------------\n"];
+	
+	[content appendString:@"\nSoftware: SDWebImage\n"];
+	[content appendString:@"Copyright (c) 2016 Olivier Poitrey rs@dailymotion.com\n\n"];
+	[content appendString:MITLicense];
+	[content appendString:@"\n----------------\n"];
     [content appendString:@"\nSoftware: EAIntroView\n"];
     [content appendString:@"Copyright (c) 2013-2015 Evgeny Aleksandrov\n\n"];
     [content appendString:MITLicense];
     [content appendString:@"\n----------------\n"];
-    [content appendString:@"\nSoftware: SDWebImage\n"];
-    [content appendString:@"Copyright (c) 2016 Olivier Poitrey rs@dailymotion.com\n\n"];
-    [content appendString:MITLicense];
-    [content appendString:@"\n----------------\n"];
-    [content appendString:@"\nSoftware: iOS Charts\n"];
+	[content appendString:@"\nSoftware: iOS Charts\n"];
     [content appendString:@"\nCopyright 2016 Daniel Cohen Gindi & Philipp Jahoda\n\n"];
     [content appendString:apacheLicense];
+	[content appendString:@"\n----------------\n"];
+	[content appendString:@"\nImages\n\n"];
+	[content appendString:@"Madebyoliver: Presentation\n"];
+	[content appendString:@"Madebyoliver: Graph\n"];
+	[content appendString:@"Madebyoliver: Info\n"];
+	[content appendString:@"Madebyoliver: Financials\n"];
+	[content appendString:@"Madebyoliver: User\n"];
+	[content appendString:@"Freepik: Athletics\n"];
+	[content appendString:@"Freepik: Agreement\n"];
+	[content appendString:@"Freepik: Businessman\n"];
+	[content appendString:@"Freepik: Lily\n"];
+	[content appendString:@"Freepik: Suitcase-with-money"];
+	[content appendString:@"Freepik: Bonusprofit\n"];
+	[content appendString:@"Freepik: Cashpayment\n"];
+	[content appendString:@"Freepik: Fiscalyear\n"];
+	[content appendString:@"Freepik: Taxform\n"];
+	[content appendString:@"Roundicons: Info\n"];
 
-   
+	
 
     return content;
 }
